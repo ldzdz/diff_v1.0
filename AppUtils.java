6bc806ffc8a69bbc9880cde5fe25314829b5eda3
ldzdz <ldzgm521@163.com>
2019-08-04 16:03:44 +0800

    Add files via upload

---
+++ AppUtils.java
@@ -0,0 +1,52 @@
+package cn.com.pansky.sxzgyl.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.text.TextUtils;
+
+/**
+ *1：获取版本
+ *
+ *2：获取路径
+ */
+public class AppUtils {
+
+    public static String getVersion(Context context) {
+        try {
+            PackageManager manager = context.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static String getOldVersionPath(Context context) {
+        try {
+            ApplicationInfo appInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(), 0);
+            System.out.println("vision"+appInfo.sourceDir);
+            return appInfo.sourceDir;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void install(Context context, String apkPath) {
+        if (TextUtils.isEmpty(apkPath)) {
+            return;
+        }
+        if (!apkPath.endsWith(".apk")) {
+            return;
+        }
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setDataAndType(Uri.parse("file://" + apkPath), "application/vnd.android.package-archive");
+        context.startActivity(i);
+    }
+
+}
